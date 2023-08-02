f434d9bc1b2611f76f1ba8edefd70eb6d788aa82
dongyongkim603 <7527536-dongyongkim603@users.noreply.replit.com>
2023-08-02 01:20:35 +0000

    created a express server and an endpoint

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const axios = require('axios');
+const goodreadsApiKey = process.env['goodreadsApiKey'];
+
+// const headers = {
+//   'Authorization': `Bearer ${goodreadsApiKey}`
+// };
 
 app.use(cors());
 
@@ -12,4 +18,24 @@ app.listen(4242, () => console.log("Running on port 4242"));
 app.get('/test', (req, res) => {
   console.log("hello world")
   return res.status(200).json({ status: 200 })
+})
+
+app.get('/books', async (req, res) => {
+  const { q, page } = req.query;
+
+  try {
+    const apiUrl = 'https://www.goodreads.com/search/index.xml';
+    const response = await axios.get(apiUrl, {
+      params: {
+        q,
+        key: goodreadsApiKey,
+        'search[field]': 'all',
+      },
+    });
+
+    return res.status(200).send(response.data);
+  } catch (error) {
+    console.error('Error searching for books:', error.message);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
 })
