b1ed2807b8baeb8fc4f0e4430a068b6009b77064
dongyongkim603 <55153930+dongyongkim603@users.noreply.github.com>
2023-08-04 13:45:02 +0000

    added in authorization function
    Checks users bearer token to see is user has credentials

--- index.js
+++ index.js
@@ -3,17 +3,41 @@ const cors = require('cors');
 const app = express();
 const axios = require('axios');
 const xml2js = require('xml2js');
+
 const goodreadsApiKey = process.env['goodreadsApiKey'];
+const goodreadsApiSecret = process.env['goodreadsApiSecret'];
 
 app.use(cors());
 
-app.use(express.static("."));
-const YOUR_DOMAIN = "http://localhost:4242";
+app.use(express.static('.'));
+const YOUR_DOMAIN = 'http://localhost:4242';
 
-app.listen(4242, () => console.log("Running on port 4242"));
+app.listen(4242, () => console.log('Running on port 4242'));
 
-app.get('/books', async (req, res) => {
-  const { q, field } = req.query;
+const authorize = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  const token = authorization.split(' ')[1];
+
+  if (token !== goodreadsApiSecret) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  next();
+};
+
+app.use(authorize);
+
+app.get('/books-search', async (req, res) => {
+  const {
+    q,
+    field,
+    sort
+  } = req.query;
+
   try {
     const apiUrl = 'https://www.goodreads.com/search/index.xml';
     const response = await axios.get(apiUrl, {
@@ -24,15 +48,16 @@ app.get('/books', async (req, res) => {
       },
     });
     const responseJSON = convertXmlToJson(response.data);
-    const results = responseJSON?.GoodreadsResponse?.search[0]?.results[0]?.work;
-    
+    let results = responseJSON?.GoodreadsResponse?.search[0]?.results[0]?.work;
+
+
     return res.status(200)
       .send(results);
   } catch (error) {
     console.error('Error searching for books:', error.message);
-    return res.status(500).json({ error: 'Internal Server Error' });
+    return res.status(500).json({ error: 'Could not find book' });
   }
-})
+});
 
 function convertXmlToJson(xmlString) {
   let jsonObject = null;
